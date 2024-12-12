15eac6e938d1462927888cc3c79b62a8449aaf3a
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-12 14:55:02 +0100

    __init__.py fix LEGACY

--- backend/app/constants/infos.py
+++ backend/app/constants/infos.py
@@ -2,3 +2,4 @@ class InfoMessages:
     ROOM_OPERATION_PASS = "Operation at rooms - result succes"
     RESERVATION_OPERATION_PASS = "Operation at reservation - result succes"
     REGISTER_PASS = "Register - result succes"
+    LOGIN_SUCCES = "User auth succes"
